4fdf07c4a2692720bc011421dd9f8e967c0557bc
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-05-29 22:55:01 +0545

    ready to deploy

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <Html>
       <div className="flex items-center justify-center">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-300 rounded-full animate-spin"></div>
+        <div className="w-20 h-20 border-2 border-opacity-20 border-red-500 border-t-red-300 rounded-full animate-spin"></div>
       </div>
     </Html>
   );
