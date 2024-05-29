e895fd1aa8ff4d583ac5c0d30e90f5564e03b0d3
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-05-29 14:27:01 +0545

    changes to view and logic

--- src/models/Merry.tsx
+++ src/models/Merry.tsx
@@ -4,49 +4,50 @@ import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import shipScene from "../assets/3d/going_merry.glb";
 
-const Merry = (props: any) => {
+const Merry = ({ ...props }: any) => {
   const shipRef = useRef<any>();
   const { scene, animations } = useGLTF(shipScene);
   const { actions } = useAnimations(animations, shipRef);
-
   console.log(actions);
 
   useFrame(({ clock }) => {
     if (shipRef.current) {
       // Update the Y position to simulate bird-like motion using a sine wave
-      shipRef.current.position.y = Math.sin(clock.elapsedTime) * 0.1 - 10;
+      shipRef.current.rotation.z = Math.sin(clock.elapsedTime) * 0.1;
 
       // Update the X and Z positions based on the direction
 
-      if (((shipRef.current.rotation.y * 180) / Math.PI) % 360 < 90) {
-        shipRef.current.position.x -= 0.2 * Math.PI;
-        shipRef.current.rotation.y += 0.1 / Math.PI;
-        shipRef.current.position.z += 0.2 * Math.PI;
-      }
-      if (
-        ((shipRef.current.rotation.y * 180) / Math.PI) % 360 > 90 &&
-        ((shipRef.current.rotation.y * 180) / Math.PI) % 360 < 180
-      ) {
-        shipRef.current.position.x += 0.2 * Math.PI;
-        shipRef.current.rotation.y += 0.1 / Math.PI;
-        shipRef.current.position.z += 0.2 * Math.PI;
-      }
-      if (
-        ((shipRef.current.rotation.y * 180) / Math.PI) % 360 > 180 &&
-        ((shipRef.current.rotation.y * 180) / Math.PI) % 360 < 270
-      ) {
-        shipRef.current.position.x += 0.2 * Math.PI;
-        shipRef.current.rotation.y += 0.1 / Math.PI;
-        shipRef.current.position.z -= 0.2 * Math.PI;
-      }
-      if (
-        ((shipRef.current.rotation.y * 180) / Math.PI) % 360 > 270 &&
-        ((shipRef.current.rotation.y * 180) / Math.PI) % 360 < 360
-      ) {
-        shipRef.current.position.x -= 0.2 * Math.PI;
-        shipRef.current.rotation.y += 0.1 / Math.PI;
-        shipRef.current.position.z -= 0.2 * Math.PI;
-      }
+      // Rotate the ship based on the direction
+      shipRef.current.rotation.y += 0.001;
+
+      // if (((shipRef.current.rotation.y * 180) / Math.PI) % 360 < 90) {
+      //   shipRef.current.rotation.y += 0.1 / Math.PI;
+      //   shipRef.current.position.z += 0.2 * Math.PI;
+      // }
+      // if (
+      //   ((shipRef.current.rotation.y * 180) / Math.PI) % 360 > 90 &&
+      //   ((shipRef.current.rotation.y * 180) / Math.PI) % 360 < 180
+      // ) {
+      //   shipRef.current.position.x += 0.2 * Math.PI;
+      //   shipRef.current.rotation.y += 0.1 / Math.PI;
+      //   shipRef.current.position.z += 0.2 * Math.PI;
+      // }
+      // if (
+      //   ((shipRef.current.rotation.y * 180) / Math.PI) % 360 > 180 &&
+      //   ((shipRef.current.rotation.y * 180) / Math.PI) % 360 < 270
+      // ) {
+      //   shipRef.current.position.x += 0.2 * Math.PI;
+      //   shipRef.current.rotation.y += 0.1 / Math.PI;
+      //   shipRef.current.position.z -= 0.2 * Math.PI;
+      // }
+      // if (
+      //   ((shipRef.current.rotation.y * 180) / Math.PI) % 360 > 270 &&
+      //   ((shipRef.current.rotation.y * 180) / Math.PI) % 360 < 360
+      // ) {
+      //   shipRef.current.position.x -= 0.2 * Math.PI;
+      //   shipRef.current.rotation.y += 0.1 / Math.PI;
+      //   shipRef.current.position.z -= 0.2 * Math.PI;
+      // }
     }
   });
   return (
--- src/modules/island/index.tsx
+++ src/modules/island/index.tsx
@@ -1,12 +1,15 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense, useState } from "react";
 import Loader from "../../components/Loader";
 import Beach from "../../models/Beach";
-import Sky from "../../models/Sky";
 import Merry from "../../models/Merry";
+import Sky from "../../models/Sky";
+import merryImg from "../../assets/Going_Merry_Anime_Concept_Art.webp";
 
 const Island = () => {
   const [isRotating, setIsRotating] = useState(false);
+  const [merry, setMerry] = useState(false);
   const [, setCurrentStage] = useState(0);
   const adjustBeachForScreenSize = () => {
     let screenScale = null;
@@ -17,7 +20,7 @@ const Island = () => {
       screenScale = [0.9, 0.9, 0.9];
       screenPosition = [0, -6.5, -43];
     } else {
-      screenScale = [1.5, 1.5, 1.5];
+      screenScale = [10, 10, 10];
       screenPosition = [0, -6.5, -43];
     }
     return [screenScale, screenPosition, rotation];
@@ -32,8 +35,8 @@ const Island = () => {
       screenScale = [0.5, 0.5, 0.5];
       screenPosition = [0, -11.3, -20];
     } else {
-      screenScale = [1, 1, 1];
-      screenPosition = [0, -13.3, -15];
+      screenScale = [15, 15, 15];
+      screenPosition = [150, -50.3, 80];
     }
     return [screenScale, screenPosition, rotation];
   };
@@ -42,13 +45,101 @@ const Island = () => {
     adjustBeachForScreenSize();
 
   const [shipScale, shipPosition, shipRotation] = adjustShipForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
+      <div
+        className={`fixed top-0 w-80 h-screen bg-white p-4 z-[10000000] transition-all duration-300 ${
+          merry ? "" : "-translate-x-full"
+        }`}
+      >
+        <img src={merryImg} alt="The Going Merry Image" />
+        <h1 className="text-3xl font-bold text-black mt-4">The Going Merry</h1>
+
+        <section className="overflow-y-scroll">
+          <h2>Creation and Introduction</h2>
+          <p>
+            The Going Merry, also known simply as the Merry, was designed and
+            built by Merry, a loyal servant of Kaya, a wealthy young girl in
+            Syrup Village. The ship was given to the Straw Hat Pirates as a gift
+            from Kaya after they saved her from the pirate captain Kuro. It
+            officially became their vessel and was named the Going Merry.
+          </p>
+          <h2>Adventures and Modifications</h2>
+          <p>
+            <strong>East Blue Saga:</strong> The Going Merry was introduced in
+            the East Blue Saga and served as the Straw Hat Pirates' primary mode
+            of transportation, carrying them through their initial adventures
+            and battles.
+          </p>
+          <p>
+            <strong>Grand Line:</strong> The ship continued to be the home and
+            vehicle for the crew as they entered the Grand Line. It was modified
+            several times to suit their needs, including the addition of a
+            cannon and other improvements by Usopp.
+          </p>
+          <h2>Significant Events</h2>
+          <p>
+            <strong>Skypiea Arc:</strong> The Going Merry managed to reach
+            Skypiea, the sky island, where it sustained significant damage.
+          </p>
+          <p>
+            <strong>Water 7 and Enies Lobby:</strong> The ship's condition
+            deteriorated further due to the harsh environments and battles. In
+            Water 7, it was revealed that the Going Merry was beyond repair,
+            leading to an emotional farewell.
+          </p>
+          <h2>Spirit of the Going Merry</h2>
+          <p>
+            <strong>Klabautermann:</strong> The ship was shown to have a spirit
+            known as a Klabautermann, which appeared to Usopp and Luffy,
+            expressing the ship's gratitude and loyalty to the crew.
+          </p>
+          <p>
+            <strong>Enies Lobby:</strong> Despite being declared unseaworthy,
+            the Going Merry miraculously arrived to rescue the crew from Enies
+            Lobby, piloted by its spirit. This moment was one of the most
+            emotional scenes in the series.
+          </p>
+          <h2>Final Farewell</h2>
+          <p>
+            <strong>Viking Funeral:</strong> After the Enies Lobby arc, the crew
+            held a Viking funeral for the Going Merry. They set the ship ablaze
+            and let it drift into the sea, honoring its service and the memories
+            they shared aboard it.
+          </p>
+          <p>
+            <strong>Emotional Impact:</strong> The farewell was deeply emotional
+            for both the characters and the fans, highlighting the strong bond
+            between the crew and their ship.
+          </p>
+          <h2>Legacy</h2>
+          <p>
+            <strong>Thousand Sunny:</strong> The Going Merry was succeeded by
+            the Thousand Sunny, a more advanced ship built by Franky. However,
+            the Going Merry remains a cherished memory for the Straw Hat
+            Pirates.
+          </p>
+          <p>
+            <strong>Symbol of the Crew's Journey:</strong> The Going Merry
+            symbolizes the early adventures, growth, and camaraderie of the
+            Straw Hat Pirates, marking an important chapter in their journey.
+          </p>
+          <h2>Summary</h2>
+          <p>
+            The Going Merry's life is a poignant tale of loyalty, adventure, and
+            friendship. From its humble beginnings in Syrup Village to its
+            heroic end at Enies Lobby, the ship played a central role in the
+            Straw Hat Pirates' journey, leaving a lasting legacy in the world of
+            "One Piece."
+          </p>
+        </section>
+      </div>
       <Canvas
         className={`w-full h-screen bg-transparent ${
           isRotating ? "cursor-grabbing" : "cursor-grab"
         }`}
-        camera={{ near: 0.1, far: 1000 }}
+        camera={{ near: 0.1, far: 5000 }}
       >
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={2} />
@@ -56,7 +147,11 @@ const Island = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight groundColor={"#000000"} intensity={1} />
-          <Sky position={[0, -100, 0]} isRotating={isRotating} />
+          <Sky
+            scale={[4, 4, 4]}
+            position={[0, -100, 0]}
+            isRotating={isRotating}
+          />
           <Beach
             position={islandPosition}
             scale={islandScale}
@@ -69,7 +164,11 @@ const Island = () => {
             scale={shipScale}
             position={shipPosition}
             rotation={shipRotation}
+            onClick={() => {
+              setMerry(!merry);
+            }}
           />
+          <OrbitControls />
         </Suspense>
       </Canvas>
     </section>
